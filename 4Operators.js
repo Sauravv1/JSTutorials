96bcaa65c613db7b1a62dd48e717609d2e2a1f65
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-09 18:17:09 +0530

    Operators in JS

---
+++ 4Operators.js
@@ -0,0 +1,31 @@
+// typeof operator
+
+
+ // data types {Primitve Data Types}
+ // string "SAURAV ", "YADAV"
+ //Number [1,2,3,4,5,........]
+ //Boolean [Have only two values ]
+ //Undefined 
+ //Null
+
+ // less used
+ // BigInt
+ // Symbol 
+
+ let age = 22;
+ let firstName = "SAURAV";
+ console.log(typeof age);
+ // it will display the typeof value of age that is number
+
+ // 1} CONVERT NUMBER TO STRING
+ //To change number to string, add an empty string to the number eg:
+
+ // also can be converted by adding String before the variable 
+// age = String(age);
+ age = age + "";
+// can be checked by 
+console.log(typeof (age));
+
+// 2} String To Number
+//To change a number to string , just add + sign before the number
+// firstname = Number(firstName);
