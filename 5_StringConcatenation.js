7a7bbe2eee849b7491a72e0aeae0476a30e29553
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-10 23:31:41 +0530

    Adding string concatenation in JS

---
+++ 5_StringConcatenation.js
@@ -0,0 +1,18 @@
+// string concatenation 
+
+/*
+let String1 = "Saurav";
+let String2 = "Yadav";
+
+let myString = String1 + " " + String2;
+console.log(myString);
+*/
+
+// Template String
+let age = 21;
+let firstName = "Saurav";
+let lastName = "Yadav ";
+
+console.log(`My name is ${firstName + " " + lastName}and my age is ${age}` );
+
+
