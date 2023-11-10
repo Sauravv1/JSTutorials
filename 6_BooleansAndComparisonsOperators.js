705fe2b15bbcab53a9cb21259c24b2259b8eebe0
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-11 00:18:41 +0530

    Booleans and compOperators

---
+++ 6_BooleansAndComparisonsOperators.js
@@ -0,0 +1,18 @@
+// Booleans and comparison operators 
+// Boooleans have two values : true and false 
+
+
+// Comparison operators
+// 1] ==
+// 2] === 
+
+let num1 = "8";
+let num2 = 8;
+console.log(num1 === num2);
+
+//  In JS == checks the value of variables and not the data type [ALLOWED ONLY IN JS]
+//  If the values are same and data type is different then it will display [TRUE]
+
+// === is used to check the value as well as datatype in JS 
+
+// != vs !==
