5ca21763efd3e277e09b03de7c26cba2014d40ee
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-11 00:35:18 +0530

    If and Else statements with truthy and falsy values

---
+++ IfAndElse.js
@@ -0,0 +1,20 @@
+//falsy  value 
+
+//false
+//null
+//undefined
+//""
+// 0
+
+// except falsy values all other values are truthy values 
+
+// if else statement 
+
+let age = 0;
+
+if(age>=18){
+    console.log("Every citizen can vote")
+}
+else{
+    console.log("You cannot vote");
+}
