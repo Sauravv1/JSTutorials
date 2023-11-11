e84b162b6e0473992201eeb2e0842f8952289479
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-11 12:35:01 +0530

    And \ OR operator

--- 7_IfAndElse.js
+++ 7_IfAndElse.js
@@ -9,7 +9,7 @@
 // except falsy values all other values are truthy values 
 
 // if else statement 
-
+/*
 let age = 0;
 
 if(age>=18){
@@ -17,4 +17,25 @@ if(age>=18){
 }
 else{
     console.log("You cannot vote");
+}
+*/
+
+// NESTED IF ELSE 
+
+// we write another if else statement into else block eg:
+
+let winningNumber = 22;
+let userGuess = +prompt("You picked");  // to execute this write this code in .html extension
+
+if(userGuess === winningNumber){
+    console.log("You picked a right number");
+}
+else{
+    if(userGuess<winningNumber){
+        console.log("You picked a lesser number");
+    }
+    else{
+        console.log("you picked a higher number");
+    }
+    
 }
---
+++ 8AndOrOperator.js
@@ -0,0 +1,33 @@
+// AND & OR Operator 
+// AND [when both conditions are true then code will get executed from if block otherwiswe 
+// else block will get executed]
+
+/*
+let age = 22;
+let firstName = "SAURAV";
+
+if(age>18 && firstName[0]==="S"){
+    console.log("both conditions are satisfied");
+}
+else{
+    console.log("code will execute from if block");
+}
+*/
+
+
+//OR OPERATOR
+
+//when both conditions are false then OR is false
+//If any one condition is true in OR then code fromm if block will get executed otherwise else block
+
+/*
+let age = 15;
+let firstName = "SAURAV";
+
+if(age>18 || firstName[0]==="S"){
+    console.log("Age is above 18 and name starts with S");
+}
+else{
+    console.log("inside else ");
+}
+*/
