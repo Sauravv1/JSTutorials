97f5b23a1aa82445c081d32b2d75305e0b2776b1
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-11 13:06:49 +0530

    else if added

--- 7_IfAndElse.js
+++ 7_IfAndElse.js
@@ -24,7 +24,7 @@ else{
 
 // we write another if else statement into else block eg:
 // prompt box is used to take an input from user 
-
+/*
 let winningNumber = 22;
 let userGuess = +prompt("You picked");  // to execute this write this code in .html extension
 
@@ -39,4 +39,41 @@ else{
         console.log("you picked a higher number");
     }
     
+}
+
+*/
+
+ // else if condition 
+//if 
+// else if
+// else if 
+// else
+
+/*
+ In  else if condtion, if one condition is found out to be true then the rest of the conditions 
+are not checked further.
+
+*/
+
+//Eg:->
+
+let score = 43;
+if(score<33){
+    console.log("Fail");
+}
+else if(score>=33 && score<50){
+    console.log("Passed with C class");
+}
+else if(score>=50 && score<70){
+    console.log("Passed with B class");
+}
+else if(score>=70 && score<90){
+    console.log("Passed with A class");
+
+}
+else if(score>=90 && score<95){
+    console.log("passed with distinction");
+}
+else{
+    console.log("EXCEPTIONAL!! ");
 }
