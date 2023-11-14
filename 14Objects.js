afe9e0b6b3fb5270da1e8db2aa516072eb53dfe2
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-14 23:48:32 +0530

    14 Objects in JS

---
+++ 14Objects.js
@@ -0,0 +1,63 @@
+//Objects are of reference data type
+// Arrays are good but not sufficient
+// Objects are iused to store real world data 
+// stores key value pair
+// Dont have index 
+// objects are stored in { curly braces }
+// we can create an array inside an object 
+
+// #How to create Object
+
+//const person = {name:"SAURAV YADAV ", age: 21};
+//console.log(person);
+
+// # How to access data  from an Object
+// variable.value,
+
+//console.log(person.name);
+//console.log(person.age);
+
+/* We can create object with multiple key value pairs
+
+const person = {
+    name:"Saurav",
+    surname:"Yadav",
+    Mobno:1122336655,
+    hobbies:["playing","reading"]
+}
+
+//console.log(person.name);
+
+// # how to add key value  pair in objects 
+
+
+person.gender = "Male";
+console.log(person);
+
+*/ 
+
+
+/*
+
+# Bracket vs dot notation
+In JS the key is by default in string so we do not have to write the key in string 
+eg to access surname of person by bracket notation
+
+
+const person = {
+    name:"Saurav",
+    surname:"Yadav",
+    Mobno:1122336655,
+    hobbies:["playing","reading"]
+}
+
+console.log(person["surname"]);
+
+
+ # how to add key value  pair in objects 
+
+
+person["gender"] = "Male";
+console.log(person["gender"];
+
+*/
