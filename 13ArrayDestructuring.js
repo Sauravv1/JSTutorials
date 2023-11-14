7aab268f6c718e28f632c7329eca093a4d5299c7
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-14 17:54:32 +0530

    Array Destructuring

---
+++ 13ArrayDestructuring.js
@@ -0,0 +1,13 @@
+// Array destructuring
+ 
+let val = ["value1","value2"];
+//let myVal1 = val[0];
+//let myVal2 = val[1];
+
+
+
+// shortcut 
+[myVal1,myVal2] = val; // this can be used instead of line 4 & 5
+// these acts as normal variables only 
+console.log("Value of myVal1 is :",myVal1);
+console.log("Value of myVal2 is :",myVal2);
