6bd51696bcb8e94d620660904a6c4c7892df37b6
Sauravv1 <Sauravyadav9284@gmail.com>
2023-11-23 12:24:30 +0530

    Function declaration,Arrow function,Function expression

---
+++ 16Functions.js
@@ -0,0 +1,61 @@
+// Function is used to do some work 
+// Function is used to replace a repetative block of code in entire code 
+// By declaring and calling a function we can replace a huge block of code 
+// we can create more than one function in a code 
+// syntax:
+// function declaratiion
+/*
+
+function name (){
+
+}
+
+
+// In thsi way we create a function 
+
+//EG:
+
+function calculate(num1,num2){
+    //return (num1+num2);
+    return (num1*num2);
+}
+
+console.log(calculate(8,6));
+
+*/
+
+// ODD or EVEN number
+/*
+
+function isEven(num){
+    if(num%2===0){
+        return true;
+    }
+    return false;
+}
+console.log(isEven(8));
+
+*/
+
+//FUNCTION EXPRESSION 
+
+//It is same as normal function 
+//Syntax:
+
+/* variable(let,const) function name = function(){
+    // code to be executed
+ }
+
+ function name();
+
+ */
+
+ // ARROW FUNCTIONS
+ // It is almost similar tof function expression
+ // Remove function keyword , add => after paranthesis
+ // SYNTAX :
+ /*
+  (const,let) functionName = ()=>{
+     code to be executed
+ } 
+ */
